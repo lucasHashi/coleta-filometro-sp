76420ecf2b5b595977937aad673363904b996b61
Lucas Belmonte <lucashashibelmonte@gmail.com>
2021-06-27 15:21:13 -0300

    Teste com outra forma de config do Selenium

--- coleta_filometro.py
+++ coleta_filometro.py
@@ -19,13 +19,23 @@ def main():
 
 def coletar_filas_agora():
     if(not MODO_DE_TESTES):
-        # Para rodar no Heroku
+        GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+        CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+        
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
-        driver_filometro = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.binary_location = GOOGLE_CHROME_PATH
+        
+        driver_filometro = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+        
+        # Para rodar no Heroku
+        #chrome_options = webdriver.ChromeOptions()
+        #chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        #chrome_options.add_argument("--headless")
+        #chrome_options.add_argument("--disable-dev-shm-usage")
+        #chrome_options.add_argument("--no-sandbox")
+        #driver_filometro = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     else:
         # Pra rodar em casa
         driver_filometro = webdriver.Chrome('./chromedriver')
